146e6f65a0125c0d7e4ff92a39f0ec281175ad60
Richard Shade <rshade@rightscale.com>
2015-02-19 09:21:40 -0600

    adding new timeouts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
 node.default[:docker_registry][:config][:listen_port]=80
+node.deafult['docker']['image_cmd_timeout'] = 1800
+node.default['docker']['container_cmd_timeout'] = 120
+node.default['docker']['registry_cmd_timeout'] = 120
